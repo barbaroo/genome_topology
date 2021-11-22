ffe209b8a80d9904ceaaf320e0481f1c25c27063
Barbara Scalvini <b.scalvini@lacdr.leidenuniv.nl>
2021-11-22 17:26:43 +0100

    finish cumulative analysis, add population Hi-C CT code

--- functions/plotting_tools.py
+++ functions/plotting_tools.py
@@ -5,6 +5,7 @@ Created on Fri Nov  5 14:32:40 2021
 @author: scalvinib
 """
 import matplotlib.pyplot as plt
+import numpy as np
 
 def set_layout(SMALL_SIZE = 13, MEDIUM_SIZE = 14, BIGGER_SIZE = 20):
 
@@ -26,3 +27,22 @@ def save_figures(plot,path, name_file, method=None):
         
     plot.savefig('{}.jpg'.format(name_figure), bbox_inches='tight')
     plot.savefig('{}.eps'.format(name_figure), bbox_inches='tight')
+    
+    
+def change_width(ax, new_value) :
+    for patch in ax.patches :
+        current_width = patch.get_width()
+        diff = current_width - new_value
+
+        # we change the bar width
+        patch.set_width(new_value)
+
+        # we recenter the bar
+        patch.set_x(patch.get_x() + diff * .5)
+        
+        
+        
+def plot_vertical_line(threshold,y):
+    thresh_line_y=np.linspace(-0.5,2,len(y))
+    thresh_line_x=np.ones(len(y))*threshold
+    return thresh_line_x,thresh_line_y
