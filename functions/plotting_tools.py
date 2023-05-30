782c8639ebd2bd03dffa967f871692c4c494195c
Barbaroo <barbaralongview@gmail.com>
2023-05-30 22:06:14 +0100

    remove bulk analysis for curaxin

--- functions/plotting_tools.py
+++ functions/plotting_tools.py
@@ -6,6 +6,8 @@ Created on Fri Nov  5 14:32:40 2021
 """
 import matplotlib.pyplot as plt
 import numpy as np
+from matplotlib.colors import ListedColormap
+import warnings
 
 def set_layout(SMALL_SIZE = 13, MEDIUM_SIZE = 14, BIGGER_SIZE = 20):
 
@@ -46,3 +48,30 @@ def plot_vertical_line(threshold,y):
     thresh_line_y=np.linspace(-0.5,2,len(y))
     thresh_line_x=np.ones(len(y))*threshold
     return thresh_line_x,thresh_line_y
+
+
+
+def matrix_plot(mat,protid):
+
+    #create custom colormap
+    newcolors = np.array([[218/255, 219/255, 228/255,1], #grey - 
+                      [131/255, 139/255, 197/255,1],    #purple S
+                      [172/255,200/255,247/255,1],       #blue P
+                      [174/255,213/255,129/255,1],         #mint green P-1
+                      [186/255, 155/255, 201/255,1],        #red purple - X
+                      [172/255, 200/255, 247/255,1],        #blue P
+                      [174/255, 213/255, 129/255,1],        #mint green
+                      [131/255,139/255, 197/255,1]])        #purple S
+    newcmp = ListedColormap(newcolors)
+
+    fig, ax = plt.subplots()
+    color = plt.get_cmap(newcmp, 8)
+
+    #plot data
+    pngmat = plt.imshow(mat,cmap=color,vmin = np.min(mat)-.5, vmax = np.max(mat)+.5)
+    ax.set_title(protid)
+    ax.tick_params(labelleft = False,labelbottom = False,bottom = False,left= False)
+    cbar = fig.colorbar(pngmat)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        cbar.ax.set_yticklabels(['-','S','P','P-1','X','CP','CP-1','CS'])
